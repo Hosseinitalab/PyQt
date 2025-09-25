d410320fcaed0177d9d5df05e9452e51964bf527
Behzad_Hosseinitalab <hosseinitalabb@gmail.com>
2025-09-25 11:37:07 +0330

    removed unnecessary buttons.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import Modules
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
+from random import choice
 
 # Main App Object and Settings
 app = QApplication([])
@@ -12,11 +13,11 @@ main_window.resize(300, 200)
 title = QLabel('Random Keywords')
 text_1 = QLabel('?')
 text_2 = QLabel('?')
-text_3 = QLabel('?')
 
 button_1 = QPushButton('Click Me')
-button_2 = QPushButton('Click Me')
-button_3 = QPushButton('Click Me')
+button_2 = QPushButton('Eraser')
+
+word_list = ['Behzad', 'Mohammad Reza', 'Ahmad Reaz', 'Zahra', 'Mobina', 'Farzaneh', 'Negar', 'Reza', 'Paria']
 
 # All Design Here
 master_layout = QVBoxLayout()
@@ -27,17 +28,26 @@ row_3 = QHBoxLayout()
 row_1.addWidget(title, alignment=Qt.AlignCenter)
 row_2.addWidget(text_1, alignment=Qt.AlignCenter)
 row_2.addWidget(text_2, alignment=Qt.AlignCenter)
-row_2.addWidget(text_3, alignment=Qt.AlignCenter)
 row_3.addWidget(button_1)
 row_3.addWidget(button_2)
-row_3.addWidget(button_3)
 
 master_layout.addLayout(row_1)
 master_layout.addLayout(row_2)
 master_layout.addLayout(row_3)
 
-# Events
 main_window.setLayout(master_layout)
+
+# create Functions
+def random_word_1():
+    text_1.setText(choice(word_list))
+    text_2.setText(choice(word_list))
+def eraser():
+    text_1.setText('?')
+    text_2.setText('?')
+
+# Events
+button_1.clicked.connect(random_word_1)
+button_2.clicked.connect(eraser)
 
 # Show/Run The App
 main_window.show()
