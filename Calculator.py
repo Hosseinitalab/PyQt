da6946fd8cf35c9853cec6979743bbe60145f49d
Behzad_Hosseinitalab <hosseinitalabb@gmail.com>
2025-10-02 09:35:06 +0330

    Calculator app turned into class-based one

--- Calculator.py
+++ Calculator.py
@@ -1,73 +1,79 @@
-# Import Modules
 from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QGridLayout
 
-# Main App Object and Settings
-app = QApplication([])
-main_window = QWidget()
-main_window.setWindowTitle('Calculator App')
-main_window.resize(250, 300)
+class CalcApp(QWidget):
+    """ Calculator Class """
 
-# Create All App Objects
-result = QLineEdit()
-grid = QGridLayout()
-buttons = [
-    '7', '8', '9', '/',
-    '4', '5', '6', '*',
-    '1', '2', '3', '-',
-    '0', '.', '=', '+'
-]
-clear = QPushButton('Clear')
-delete = QPushButton('<')
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Calculator App')
+        self.resize(250, 300)
 
-# create Functions
-def button_click():
-    button = app.sender() # determines which button was clicked
-    text = button.text()
-    
-    match text:
-        case '=':
-            symbol = result.text()
-            try:
-                res = eval(symbol)
-                result.setText(str(res))
-            except Exception as e:
-                print('Error: ', e)
-        case 'Clear':
-            result.clear()
-        case '<':
-            current_value = result.text()
-            result.setText(current_value[:-1])
-        case _:
-            current_value = result.text()
-            result.setText(current_value + text)
+        # Design
+        self.result = QLineEdit()
+        self.grid = QGridLayout()
+        self.clear = QPushButton('Clear')
+        self.delete = QPushButton('<')
+        self.button()
 
-# The rest of the create object
-row = 0
-col = 0
-for text in buttons:
-    button = QPushButton(text)
-    button.clicked.connect(button_click)
-    grid.addWidget(button, row, col)
-    col += 1
-    if col > 3:
-        row += 1
+        # Layout
+        master_layout = QVBoxLayout()
+        button_row = QHBoxLayout()
+        master_layout.addWidget(self.result)
+        master_layout.addLayout(self.grid)
+        button_row.addWidget(self.clear)
+        button_row.addWidget(self.delete)
+        master_layout.addLayout(button_row)
+        self.setLayout(master_layout)
+
+        # Events
+        self.clear.clicked.connect(self.button_click)
+        self.delete.clicked.connect(self.button_click)
+
+    def button(self):
+        """ Button Creation """
+
+        buttons = [
+            '7', '8', '9', '/',
+            '4', '5', '6', '*',
+            '1', '2', '3', '-',
+            '0', '.', '=', '+'
+        ]
+        row = 0
         col = 0
+        for text in buttons:
+            button = QPushButton(text)
+            button.clicked.connect(self.button_click)
+            self.grid.addWidget(button, row, col)
+            col += 1
+            if col > 3:
+                row += 1
+                col = 0
 
-# All Design Here
-master_layout = QVBoxLayout()
-button_row = QHBoxLayout()
-master_layout.addWidget(result)
-master_layout.addLayout(grid)
-button_row.addWidget(clear)
-button_row.addWidget(delete)
+    def button_click(self):
+        """ Button Event """
 
-master_layout.addLayout(button_row)
-main_window.setLayout(master_layout)
+        button = app.sender() # determines which button was clicked
+        text = button.text()
+        
+        match text:
+            case '=':
+                symbol = self.result.text()
+                try:
+                    res = eval(symbol)
+                    self.result.setText(str(res))
+                except Exception as e:
+                    print('Error: ', e)
+            case 'Clear':
+                self.result.clear()
+            case '<':
+                current_value = self.result.text()
+                self.result.setText(current_value[:-1])
+            case _:
+                current_value = self.result.text()
+                self.result.setText(current_value + text)
 
-# Events
-clear.clicked.connect(button_click)
-delete.clicked.connect(button_click)
-
-# Show/Run The App
-main_window.show()
-app.exec_()
+if __name__ == '__main__':
+    app = QApplication([])
+    main_window = CalcApp()
+    main_window.show()
+    app.exec_()
